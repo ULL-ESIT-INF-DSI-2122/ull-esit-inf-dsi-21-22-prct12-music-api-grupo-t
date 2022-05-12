16d350eb06aa5689e089dac82d96324b302c25c2
Tanausú Falcón <alu0101320878@ull.edu.es>
2022-05-12 11:54:11 +0000

    Added Data Models

---
+++ src/models/artist.ts
@@ -0,0 +1,111 @@
+import {Song} from './song';
+import {Genre} from './genre';
+
+
+/**
+ * Artist class. Represents the owner of songs, albumns and members of a group
+ * potentially
+ */
+export class Artist {
+  /**
+   * Stores the monthly listeners of this artist, calculated in the constructor. Takes into
+   * account the songs of the group that the artist is member aswell.
+   */
+  private monthlyListeners: number;
+
+  /**
+   * Constructor
+   * @param name of the artist
+   * @param groups where the artist is member
+   * @param genres of the songs of the artist
+   * @param albums that the artist has made
+   * @param publishedSongs stores the songs that the artist has published
+   */
+  constructor(private name: string, private genres: Genre[] = [],
+    private publishedSongs: Song[] = []) {
+
+    let listeners = 0;
+     // Counts the views of all of the artist`s songs
+    this.publishedSongs.forEach((song) => {
+     listeners += song.getViews();
+    });
+    this.monthlyListeners = listeners;
+  }
+
+  /**
+   * @returns the name of the artist
+   */
+  public getName(): string {
+    return this.name;
+  }
+
+  /**
+   * @returns the genres of the songs that the artist has made
+   */
+  public getGenres(): Genre[] {
+    return this.genres;
+  }
+  
+  public getSongs(): Song[] {
+    return this.publishedSongs;
+  }
+
+  /**
+   * @returns all the songs that the artist has realeased as a string
+   */
+  public getPublishedSongs(): string[] {
+    let songsNames: string[] = [];
+    this.publishedSongs.forEach(song => {
+      songsNames.push(song.getName());
+    });
+    return songsNames;
+  }
+
+  /**
+   * @returns the number of the monthly listeners of the artist
+   */
+  public getMonthlyListeners(): number {
+    return this.monthlyListeners;
+  }
+
+  /**
+   * Add's a song to the artist
+   * @param song that will be added
+   */
+  public addPublishedSong(song: Song) {
+    this.publishedSongs.push(song);
+  }
+
+  /**
+   * Add's an album to the artist
+   * @param genre that will be added
+   */
+   public addGenre(genre: Genre) {
+    this.genres.push(genre);
+  }
+
+  /**
+   * Removes a genre from an artist
+   * @param genreName genre to remove
+   */
+  public removeGenre(genreName: Genre): void {
+    this.genres.forEach((genre, i) => {
+      if (genre === genreName) {
+        this.genres.splice(i, 1);
+      }
+    });
+  }
+  
+  /**
+   * Removes a song from an artist
+   * @param songName song to remove
+   */
+  public removeSong(songName: string): void {
+    for(let i=0; i < this.publishedSongs.length;i++){
+      if (this.publishedSongs[i].getName() === songName) {
+        console.log(this.publishedSongs[i])
+        delete(this.publishedSongs[i])
+      }
+    }
+  }
+}
---
+++ src/models/song.ts
@@ -0,0 +1,145 @@
+import {Genre} from './genre'
+import {Artist} from './artist'
+
+
+/**
+ * Type that shows the duration of a song in the 'minute:second'
+ * format
+ */
+export type Duration = {
+  minutes: number,
+  seconds: number
+}
+
+/*
+ * Song class that represents all the songs 
+ */
+export class Song {
+  /**
+   * Constructor
+   * @param name of the song 
+   * @param author of the song
+   * @param duration of the song
+   * @param genres of the song
+   * @param isSingle boolean that shows if the songs is a single
+   * @param views of the song
+   */
+  constructor(private name: string, private author: Artist, 
+    private duration: Duration = {minutes: 0, seconds: 0}, private genres: Genre[] = [], 
+    private isSingle: boolean = true, private views: number = 0) {}
+
+  /**
+   * @returns the name of the song
+   */
+  public getName(): string {
+    return this.name;
+  }
+
+  /**
+   * @returns the author of the song
+   */
+  public getAuthor() {
+    return this.author;
+  }
+
+  /**
+   * @returns the duration of the song
+   */
+  public getDuration(): Duration {
+    return this.duration;
+  }
+
+  /**
+   * @returns the genre of the song
+   */
+  public getGenres() {
+    return this.genres;
+  }
+
+  /**
+   * @returns if the song is a single
+   */
+  public getIsSingle() {
+    return this.isSingle;
+  }
+
+  /**
+   * @returns the views of the song
+   */
+  public getViews() {
+    return this.views;
+  }
+
+  /**
+   * Changes de name of the song
+   * @param name that will be updated
+   */
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  /**
+   * Changes de author of the song
+   * @param author that will be updated
+   */
+  public setAuthor(author: Artist) {
+    this.author = author;
+  }
+
+  /**
+   * Changes de duration of the song
+   * @param duration that will be updated
+   */
+  public setDuration(duration: Duration) {
+    this.duration = duration;
+  }
+
+  /**
+   * Changes de genres of the song
+   * @param genres that will be updated
+   */
+  public setGenres(genres: Genre[]) {
+    this.genres = genres;
+  }
+
+  /**
+   * Changes de isSigle of the song
+   * @param isSigle that will be updated
+   */
+  public setIsSingle(isSigle: boolean) {
+    this.isSingle = isSigle;
+  }
+
+  /**
+   * Changes de views of the song
+   * @param views that will be updated
+   */
+  public setViews(views: number) {
+    this.views = views;
+  }
+
+  /**
+   * Removes a genre from a song
+   * @param genreName name of the genre that will be removed
+   */
+  public removeGenre(genreName: Genre): void {
+    this.genres.forEach((genre, i) => {
+      if (genre === genreName) {
+        this.genres.splice(i, 1);
+      }
+    });
+  }
+
+  /**
+   * Returns the information of the song as a string
+   */
+  public asString(): string {
+    let genresString = '';
+    this.genres.forEach(genre => {
+      genresString += `${genre} `;
+    });
+    let formatedSong = `${this.name} - Author: ${this.author}, Duration: ${this.duration.minutes}:${this.duration.seconds}, ` +
+      `Views: ${this.views}, Genres: ${genresString}`;
+    return formatedSong;
+  }
+}
