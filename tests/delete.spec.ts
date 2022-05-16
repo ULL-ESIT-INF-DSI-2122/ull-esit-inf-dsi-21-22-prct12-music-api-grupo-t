258fb8433f24b486decf3ed6a85a87a15c7be6cb
alu0101203980 <alu0101203980@ull.edu.es>
2022-05-16 17:28:41 +0000

    Pruebas PATCH

--- tests/delete.spec.ts
+++ tests/delete.spec.ts
@@ -11,7 +11,7 @@ describe('Petición DELETE', () => {
   it('Debe borrar un artista', (done) => {
     chai.request(url).del('/artist?name=Artist1').end((err, res) => {
       if (err) {
-        throw new Error('An error has occur');
+        throw new Error('ERROR al borrar el artista');
       }
       expect(res).to.have.status(200);
       chai.request(url).get('/artist?name=Artist1').end((err, res) => {
@@ -26,7 +26,7 @@ describe('Petición DELETE', () => {
   it('Debe borrar una canción', (done) => {
     chai.request(url).del('/song?name=Song1').end((err, res) => {
       if (err) {
-        throw new Error('An error has occur');
+        throw new Error('ERROR al borrar la canción');
       }
       expect(res).to.have.status(404);
       chai.request(url).get('/song?name=Song1').end((err, res) => {
@@ -41,7 +41,7 @@ describe('Petición DELETE', () => {
   it('Debe borrar una playlist', (done) => {
     chai.request(url).del('/playlist?name=PlayList1').end((err, res) => {
       if (err) {
-        throw new Error('An error has occur');
+        throw new Error('ERROR al borrar la playlist');
       }
       expect(res).to.have.status(501);
       chai.request(url).get('/playlist?name=PlayList1').end((err, res) => {
---
+++ tests/patch.spec.ts
@@ -0,0 +1,76 @@
+import 'mocha';
+import * as chai from 'chai';
+import {expect} from 'chai';
+import 'chai-http';
+
+chai.use(require('chai-http'));
+const url= 'http://localhost:3000';
+
+describe('Petición PATCH', () => {
+  it('Debe modificar un artista', (done) => {
+    chai.request(url).patch('/artist?name=Artist1').send({
+      name: 'Artist2',
+      genres: [
+        'Pop',
+      ],
+      monthlyListeners: 4,
+    }).end((err, res) => {
+      if (err) {
+        throw new Error('ERROR al modificar el artista');
+      }
+      expect(res).to.have.status(400);
+      chai.request(url).patch('/artist?name=Artist1').send({
+        name: 'Artist2',
+        genres: [
+          'Pop',
+        ],
+        monthlyListeners: 4,
+      });
+      done();
+    });
+  });
+  it('Debe modificar una canción', (done) => {
+    chai.request(url).patch('/song?name=Song1').send({
+      name: 'Song2',
+      author: "Artist1",
+      reproductions: 8,
+      single: true,
+      duration: {min: 2, sec: 34},
+      monthlyListeners: 5,
+    }).end((err, res) => {
+      if (err) {
+        throw new Error('ERROR al modificar la canción');
+      }
+      expect(res).to.have.status(501);
+      chai.request(url).patch('/song?name=Song1').send({
+        name: 'Song2',
+        author: "Artist1",
+        reproductions: 8,
+        single: true,
+        duration: {min: 2, sec: 34},
+        monthlyListeners: 5,
+      });
+      done();
+    });
+  });
+  it('Debe modificar una playlist', (done) => {
+    chai.request(url).patch('/playlist?name=PlayList1').send({
+      name: 'PlayList2',
+      songs: [
+        'Song1',
+      ],
+    }).end((err, res) => {
+      if (err) {
+        throw new Error('ERROR al modificar la playlist');
+      }
+      expect(res).to.have.status(501);
+      chai.request(url).patch('/playlist?name=PlayList1').send({
+        name: 'PlayList2',
+        songs: [
+          'Song1',
+        ],
+      });
+      done();
+    });
+  });
+});
