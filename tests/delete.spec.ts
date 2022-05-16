f39f35031b2fc5c26975280c20f94f3d27710a64
alu0101203980 <alu0101203980@ull.edu.es>
2022-05-16 17:04:04 +0000

    Pruebas DELETE

---
+++ tests/delete.spec.ts
@@ -0,0 +1,56 @@
+import 'mocha';
+import * as chai from 'chai';
+import {expect} from 'chai';
+import 'chai-http';
+
+require('./post.spec.ts');
+chai.use(require('chai-http'));
+const url= 'http://localhost:3000';
+
+describe('Petición DELETE', () => {
+  it('Debe borrar un artista', (done) => {
+    chai.request(url).del('/artist?name=Artist1').end((err, res) => {
+      if (err) {
+        throw new Error('An error has occur');
+      }
+      expect(res).to.have.status(200);
+      chai.request(url).get('/artist?name=Artist1').end((err, res) => {
+        if (err) {
+          throw new Error('ERROR al borrar el artista');
+        }
+        expect(res).to.have.status(200);
+        done();
+      });
+    });
+  });
+  it('Debe borrar una canción', (done) => {
+    chai.request(url).del('/song?name=Song1').end((err, res) => {
+      if (err) {
+        throw new Error('An error has occur');
+      }
+      expect(res).to.have.status(404);
+      chai.request(url).get('/song?name=Song1').end((err, res) => {
+        if (err) {
+          throw new Error('ERROR al borrar la canción');
+        }
+        expect(res).to.have.status(200);
+        done();
+      });
+    });
+  });
+  it('Debe borrar una playlist', (done) => {
+    chai.request(url).del('/playlist?name=PlayList1').end((err, res) => {
+      if (err) {
+        throw new Error('An error has occur');
+      }
+      expect(res).to.have.status(501);
+      chai.request(url).get('/playlist?name=PlayList1').end((err, res) => {
+        if (err) {
+          throw new Error('ERROR al borrar la playlist');
+        }
+        expect(res).to.have.status(404);
+        done();
+      });
+    });
+  });
+});
